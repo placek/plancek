a177550a8b5f276c863ed4fef2126a45cee07487
Paweł Placzyński <placzynski.pawel@gmail.com>
2022-08-18 14:52:02 +0200

    Fixes; set up tapping delay lower

--- keyboards/planck/keymaps/plancek/keymap.c
+++ keyboards/planck/keymaps/plancek/keymap.c
@@ -294,55 +294,55 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 
 bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0: {
+  switch (index) {
+    case 0: {
 #ifdef AUDIO_ENABLE
-            static bool play_sound = false;
+      static bool play_sound = false;
 #endif
-            if (active) {
+      if (active) {
 #ifdef AUDIO_ENABLE
-                if (play_sound) { PLAY_SONG(plover_song); }
+        if (play_sound) { PLAY_SONG(plover_song); }
 #endif
-                layer_on(_ADJUST);
-            } else {
+        layer_on(_ADJUST);
+      } else {
 #ifdef AUDIO_ENABLE
-                if (play_sound) { PLAY_SONG(plover_gb_song); }
+        if (play_sound) { PLAY_SONG(plover_gb_song); }
 #endif
-                layer_off(_ADJUST);
-            }
+        layer_off(_ADJUST);
+      }
 #ifdef AUDIO_ENABLE
-            play_sound = true;
+      play_sound = true;
 #endif
-            break;
-        }
-        case 1:
-            if (active) {
-                muse_mode = true;
-            } else {
-                muse_mode = false;
-            }
+      break;
     }
-    return true;
+    case 1:
+      if (active) {
+        muse_mode = true;
+      } else {
+        muse_mode = false;
+      }
+  }
+  return true;
 }
 
 void matrix_scan_user(void) {
 #ifdef AUDIO_ENABLE
-    if (muse_mode) {
-        if (muse_counter == 0) {
-            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-            if (muse_note != last_muse_note) {
-                stop_note(compute_freq_for_midi_note(last_muse_note));
-                play_note(compute_freq_for_midi_note(muse_note), 0xF);
-                last_muse_note = muse_note;
-            }
-        }
-        muse_counter = (muse_counter + 1) % muse_tempo;
-    } else {
-        if (muse_counter) {
-            stop_all_notes();
-            muse_counter = 0;
-        }
+  if (muse_mode) {
+    if (muse_counter == 0) {
+      uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+      if (muse_note != last_muse_note) {
+        stop_note(compute_freq_for_midi_note(last_muse_note));
+        play_note(compute_freq_for_midi_note(muse_note), 0xF);
+        last_muse_note = muse_note;
+      }
     }
+    muse_counter = (muse_counter + 1) % muse_tempo;
+  } else {
+    if (muse_counter) {
+      stop_all_notes();
+      muse_counter = 0;
+    }
+  }
 #endif
 }
 
