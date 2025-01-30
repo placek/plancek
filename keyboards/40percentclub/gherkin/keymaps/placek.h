480a426a66387121c80ecaf2dc8b910c0fe5e86d
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-01-30 09:33:18 +0100

    Unify keyboard layouts
    This commit consolidates multiple keyboard layouts by removing the
    redundant `placek.c` file and updating the necessary header and keymap
    files to support a unified layout. The changes simplify the keymap
    configurations and improve the consistency of combo definitions across
    different layouts. New combo definitions and key processing logic have
    been introduced to enhance functionality while preserving existing
    features. This approach facilitates better maintainability and reduces
    the complexity associated with managing separate layouts, ultimately
    streamlining the development process.

--- keyboards/40percentclub/gherkin/keymaps/placek.h
+++ keyboards/40percentclub/gherkin/keymaps/placek.h
@@ -28,4 +28,22 @@ enum custom_keycodes {
   PRN
 };
 
+const uint16_t PROGMEM c_a_combo[]  = {CTL_A, KC_S, COMBO_END};
+const uint16_t PROGMEM btn_combo[]  = {FN3_V, FN2_BSPC, COMBO_END};
+const uint16_t PROGMEM tab_combo[]  = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM prn_combo[]  = {KC_LPRN, KC_RPRN, COMBO_END};
+const uint16_t PROGMEM rbr_combo[]  = {KC_LBRC, KC_RBRC, COMBO_END};
+const uint16_t PROGMEM cbr_combo[]  = {KC_LCBR, KC_RCBR, COMBO_END};
+const uint16_t PROGMEM caps_combo[] = {SFT_Z, SFT_ESC, COMBO_END};
+
+combo_t key_combos[] = {
+  COMBO(tab_combo, KC_TAB),
+  COMBO(c_a_combo, C_A),
+  COMBO(btn_combo, KC_BTN1),
+  COMBO(prn_combo, PRN),
+  COMBO(rbr_combo, RBR),
+  COMBO(cbr_combo, CBR),
+  COMBO(caps_combo, CW_TOGG)
+};
+
 #endif // PLACEK_H
